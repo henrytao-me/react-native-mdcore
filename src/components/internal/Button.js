ef26be943d59775aa6b6058195eefd520db07091
Henry Tao <hi@henrytao.me>
2017-06-24 08:28:08 +0700

    Bump to 0.1.3

--- src/components/internal/Button.js
+++ src/components/internal/Button.js
@@ -1,64 +1,15 @@
-/* eslint-disable */
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- * @providesModule Button
- * @flow
- */
-'use strict';
+import React, { Component } from 'react'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View
+} from 'react-native'
+import PropTypes from 'prop-types'
 
-const ColorPropType = require('ColorPropType');
-const Platform = require('Platform');
-const React = require('React');
-const PropTypes = require('prop-types');
-const StyleSheet = require('StyleSheet');
-const Text = require('Text');
-const TouchableNativeFeedback = require('TouchableNativeFeedback');
-const TouchableOpacity = require('TouchableOpacity');
-const View = require('View');
-
-const invariant = require('fbjs/lib/invariant');
-
-/**
- * A basic button component that should render nicely on any platform. Supports
- * a minimal level of customization.
- *
- * <center><img src="img/buttonExample.png"></img></center>
- *
- * If this button doesn't look right for your app, you can build your own
- * button using [TouchableOpacity](docs/touchableopacity.html)
- * or [TouchableNativeFeedback](docs/touchablenativefeedback.html).
- * For inspiration, look at the [source code for this button component](https://github.com/facebook/react-native/blob/master/Libraries/Components/Button.js).
- * Or, take a look at the [wide variety of button components built by the community](https://js.coach/react-native?search=button).
- *
- * Example usage:
- *
- * ```
- * <Button
- *   onPress={onPressLearnMore}
- *   title="Learn More"
- *   color="#841584"
- *   accessibilityLabel="Learn more about this purple button"
- * />
- * ```
- *
- */
-
-class Button extends React.Component {
-
-  props: {
-    title: string,
-    onPress: () => any,
-    color?: ?string,
-    accessibilityLabel?: ?string,
-    disabled?: ?boolean,
-    testID?: ?string,
-  };
+export default class Button extends Component {
 
   static propTypes = {
     /**
@@ -72,7 +23,7 @@ class Button extends React.Component {
     /**
      * Color of the text (iOS), or background color of the button (Android)
      */
-    color: ColorPropType,
+    color: PropTypes.any,
     /**
      * If true, disable all interactions for this component.
      */
@@ -88,37 +39,33 @@ class Button extends React.Component {
 
     buttonStyle: React.PropTypes.object,
     textStyle: React.PropTypes.object
-  };
+  }
 
   render() {
     const {
       accessibilityLabel,
       color,
       onPress,
-      title,
       disabled,
       testID,
-    } = this.props;
-    const buttonStyles = [styles.button];
-    const textStyles = [styles.text];
-    const Touchable = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+    } = this.props
+    const title = `${this.props.title}`
+    const buttonStyles = [styles.button]
+    const textStyles = [styles.text]
+    const Touchable = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity
     if (color && Platform.OS === 'ios') {
-      textStyles.push({color: color});
+      textStyles.push({ color: color })
     } else if (color) {
-      buttonStyles.push({backgroundColor: color});
+      buttonStyles.push({ backgroundColor: color })
     }
     if (disabled) {
-      buttonStyles.push(styles.buttonDisabled);
-      textStyles.push(styles.textDisabled);
+      buttonStyles.push(styles.buttonDisabled)
+      textStyles.push(styles.textDisabled)
     }
-    invariant(
-      typeof title === 'string',
-      'The title prop of a Button must be a string',
-    );
-    const formattedTitle = Platform.OS === 'android' ? title.toUpperCase() : title;
-    const accessibilityTraits = ['button'];
+    const formattedTitle = Platform.OS === 'android' ? title.toUpperCase() : title
+    const accessibilityTraits = ['button']
     if (disabled) {
-      accessibilityTraits.push('disabled');
+      accessibilityTraits.push('disabled')
     }
     buttonStyles.push(this.props.buttonStyle)
     textStyles.push(this.props.textStyle)
@@ -134,15 +81,15 @@ class Button extends React.Component {
           <Text style={textStyles} disabled={disabled}>{formattedTitle}</Text>
         </View>
       </Touchable>
-    );
+    )
   }
 }
 
 // Material design blue from https://material.google.com/style/color.html#color-color-palette
-let defaultBlue = '#2196F3';
+let defaultBlue = '#2196F3'
 if (Platform.OS === 'ios') {
   // Measured default tintColor from iOS 10
-  defaultBlue = '#007AFF';
+  defaultBlue = '#007AFF'
 }
 
 const styles = StyleSheet.create({
@@ -187,6 +134,4 @@ const styles = StyleSheet.create({
       color: '#a1a1a1',
     }
   }),
-});
-
-module.exports = Button;
+})
--- src/components/internal/ViewPager.js
+++ src/components/internal/ViewPager.js
@@ -1,79 +1,43 @@
-/* eslint-disable */
-/**
- * Copyright 2016 Facebook, Inc.
- *
- * You are hereby granted a non-exclusive, worldwide, royalty-free license to
- * use, copy, modify, and distribute this software in source code or binary
- * form for use in connection with the web services and APIs provided by
- * Facebook.
- *
- * As with any software that integrates with the Facebook platform, your use
- * of this software is subject to the Facebook Developer Principles and
- * Policies [http://developers.facebook.com/policy/]. This copyright notice
- * shall be included in all copies or substantial portions of the software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE
- *
- * @flow
- */
-'use strict';
-
-const React = require('react');
-const ReactNative = require('react-native');
-
-const {
-  View,
-  StyleSheet,
-  ScrollView,
-  ViewPagerAndroid,
+import React, { Component } from 'react'
+import {
   Platform,
-} = ReactNative;
+  ScrollView,
+  StyleSheet,
+  View,
+  ViewPagerAndroid
+} from 'react-native'
 
-type Props = {
-  count: number;
-  selectedIndex: number;
-  onSelectedIndexChange?: (index: number) => void;
-  bounces?: boolean;
-  children?: any;
-  style?: any;
-};
+export default class ViewPager extends Component {
 
-type State = {
-  width: number;
-  height: number;
-  selectedIndex: number;
-  initialSelectedIndex: number;
-  scrollingTo: ?number;
-};
+  static propTypes = {
+    bounces: PropTypes.boolean,
+    count: PropTypes.number,
+    onSelectedIndexChange: PropTypes.func,
+    selectedIndex: PropTypes.number
+  }
 
-class ViewPager extends React.Component {
-  props: Props;
-  state: State;
+  static defaultProps = {
+    onSelectedIndexChange: (_index) => { }
+  }
 
-  constructor(props: Props) {
-    super(props);
+  constructor(props) {
+    super(props)
     this.state = {
       width: 0,
       height: 0,
       selectedIndex: this.props.selectedIndex,
       initialSelectedIndex: this.props.selectedIndex || 0,
-      scrollingTo: null,
-    };
-    (this: any).handleHorizontalScroll = this.handleHorizontalScroll.bind(this);
-    (this: any).adjustCardSize = this.adjustCardSize.bind(this);
+      scrollingTo: null
+    }
+    this.handleHorizontalScroll = this.handleHorizontalScroll.bind(this)
+    this.adjustCardSize = this.adjustCardSize.bind(this)
   }
 
   render() {
     if (Platform.OS === 'ios') {
-      return this.renderIOS();
+      return this.renderIOS()
     } else {
-      return this.renderAndroid();
+      return this.renderAndroid()
     }
   }
 
@@ -101,7 +65,7 @@ class ViewPager extends React.Component {
         onLayout={this.adjustCardSize}>
         {this.renderContent()}
       </ScrollView>
-    );
+    )
   }
 
   renderAndroid() {
@@ -114,34 +78,34 @@ class ViewPager extends React.Component {
         onLayout={this.adjustCardSize}>
         {this.renderContent()}
       </ViewPagerAndroid>
-    );
+    )
   }
 
-  adjustCardSize(e: any) {
+  adjustCardSize(e) {
     this.setState({
       width: e.nativeEvent.layout.width,
       height: e.nativeEvent.layout.height,
-    });
+    })
   }
 
-  componentWillReceiveProps(nextProps: Props) {
+  componentWillReceiveProps(nextProps) {
     if (nextProps.selectedIndex !== this.state.selectedIndex) {
       if (Platform.OS === 'ios') {
         this.refs.scrollview.scrollTo({
           x: nextProps.selectedIndex * this.state.width,
           animated: true,
-        });
-        this.setState({ scrollingTo: nextProps.selectedIndex });
+        })
+        this.setState({ scrollingTo: nextProps.selectedIndex })
       } else {
-        this.refs.scrollview.setPage(nextProps.selectedIndex);
-        this.setState({ selectedIndex: nextProps.selectedIndex });
+        this.refs.scrollview.setPage(nextProps.selectedIndex)
+        this.setState({ selectedIndex: nextProps.selectedIndex })
       }
     }
   }
 
-  renderContent(): Array<ReactElement> {
-    var { width, height } = this.state;
-    var style = Platform.OS === 'ios' && styles.card;
+  renderContent() {
+    const { width, height } = this.state
+    const style = Platform.OS === 'ios' && styles.card
     if (!this.props.children) {
       return null
     }
@@ -152,23 +116,23 @@ class ViewPager extends React.Component {
     ))
   }
 
-  handleHorizontalScroll(e: any) {
-    var selectedIndex = e.nativeEvent.position;
+  handleHorizontalScroll(e) {
+    const selectedIndex = e.nativeEvent.position
     if (selectedIndex === undefined) {
       selectedIndex = Math.round(
         e.nativeEvent.contentOffset.x / this.state.width,
-      );
+      )
     }
     if (selectedIndex < 0 || selectedIndex >= this.props.count) {
-      return;
+      return
     }
     if (this.state.scrollingTo !== null && this.state.scrollingTo !== selectedIndex) {
-      return;
+      return
     }
     if (this.props.selectedIndex !== selectedIndex || this.state.scrollingTo !== null) {
-      this.setState({ selectedIndex, scrollingTo: null });
-      const { onSelectedIndexChange } = this.props;
-      onSelectedIndexChange && onSelectedIndexChange(selectedIndex);
+      this.setState({ selectedIndex, scrollingTo: null })
+      const { onSelectedIndexChange } = this.props
+      onSelectedIndexChange && onSelectedIndexChange(selectedIndex)
     }
   }
 
@@ -177,15 +141,15 @@ class ViewPager extends React.Component {
       this.refs.scrollview.scrollTo({
         x: index * this.state.width,
         animated: true,
-      });
-      this.setState({ scrollingTo: index });
+      })
+      this.setState({ scrollingTo: index })
     } else {
       this.refs.scrollview.setPage(index)
     }
   }
 }
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
@@ -196,6 +160,4 @@ var styles = StyleSheet.create({
   card: {
     backgroundColor: 'transparent',
   }
-});
-
-module.exports = ViewPager;
+})

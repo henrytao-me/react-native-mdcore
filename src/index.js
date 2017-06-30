76860413b8b93f0e2cb0bd77063f30fe7bffac01
Henry Tao <hi@henrytao.me>
2017-06-30 08:41:30 +0700

    Exported Resolver

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 export * from './components'
 
+import Resolver from './libs/resolver'
 import Theme from './theme'
 import ThemeProvider from './providers/theme'
 import * as ThemeUtils from './libs/utils'
 
 export {
+  Resolver,
   Theme,
   ThemeProvider,
   ThemeUtils

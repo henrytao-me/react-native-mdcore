117095b8bad7875549ba12d3aa62de342bcbef5b
Henry Tao <henry.tao@shopify.com>
2017-12-08 06:46:04 -0500

    Add ripple

--- src/components/Ripple.js
+++ src/components/Ripple.js
@@ -4,23 +4,27 @@ import { TouchableWithoutFeedback, View } from 'react-native'
 import PropTypes from './PropTypes'
 import ThemeComponent from './ThemeComponent'
 
+import { default as RNRipple } from './internal/Ripple'
+
 export default class Ripple extends ThemeComponent {
   static contextTypes = {
     theme: PropTypes.any
   }
 
   static propTypes = {
-    borderless: PropTypes.bool,
-    color: PropTypes.color,
-    duration: PropTypes.number,
-    opacity: PropTypes.opacity,
-    size: PropTypes.number,
+    // borderless: PropTypes.bool,
+    // color: PropTypes.color,
+    // duration: PropTypes.number,
+    // opacity: PropTypes.opacity,
+    // size: PropTypes.number,
+    radius: PropTypes.number,
     touchable: PropTypes.bool,
     onPress: PropTypes.func
   }
 
   static defaultProps = {
-    borderless: false,
+    // borderless: false,
+    radius: 0,
     touchable: true,
     onPress: () => {}
   }
@@ -31,15 +35,13 @@ export default class Ripple extends ThemeComponent {
 
   render() {
     return (
-      <TouchableWithoutFeedback onPress={this.props.onPress}>
-        <View style={this.props.style}>
-          {this.props.children}
-        </View>
-      </TouchableWithoutFeedback>
+      <RNRipple
+        onPress={this.props.onPress}
+        disabled={!this.props.touchable}
+        rippleContainerBorderRadius={this.props.radius}
+      >
+        <View style={this.props.style}>{this.props.children}</View>
+      </RNRipple>
     )
   }
 }
-
-// const Styles = StyleSheet.create(theme => {
-//   return {}
-// })
